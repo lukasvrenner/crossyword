62e93c3aca0c432a98151788a6fc1ee794d1ce0a
lukasvrenner <lukasvrenner@icloud.com>
2024-01-08 20:13:14 -0900

    improved error handling

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,25 @@ use std::fs;
 mod generate_layout;
 use crate::generate_layout::*;
 
-fn main() -> std::io::Result<()> {
-    let unformatted_words = fs::read_to_string("./words.txt")?;
+fn main() {
+    let words_file = "./words.txt";
 
     // display error to window once GUI is created
-    let formatted_words = format_words(&unformatted_words).unwrap();
+    let unformatted_words = fs::read_to_string(words_file)
+        .unwrap_or_else(|err| {
+            eprintln!("could not read {}: {}", words_file, err);
+            std::process::exit(1);
+    });
+
+    // display error to window once GUI is created
+    let formatted_words = format_words(&unformatted_words)
+        .unwrap_or_else(||{
+            eprintln!("could not parse {}", words_file);
+            std::process::exit(1);
+        });
 
     let puzzle = new_puzzle(&formatted_words).unwrap();
     println!("{:?}", puzzle);
 
-    Ok(())
 }
 
