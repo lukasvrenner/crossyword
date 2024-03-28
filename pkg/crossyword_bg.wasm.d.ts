fe0273f182c4c677e6c20d02803f28d433d873be
lukasvrenner <lukasvrenner@icloud.com>
2024-03-27 19:33:53 -0800

    added binary to repo

---
+++ pkg/crossyword_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_placedword_free(a: number): void;
+export function __wbg_get_placedword_orientation(a: number): number;
+export function __wbg_set_placedword_orientation(a: number, b: number): void;
+export function __wbg_get_placedword_xpos(a: number): number;
+export function __wbg_set_placedword_xpos(a: number, b: number): void;
+export function __wbg_get_placedword_ypos(a: number): number;
+export function __wbg_set_placedword_ypos(a: number, b: number): void;
+export function placedword_word(a: number, b: number): void;
+export function placedword_clue(a: number, b: number): void;
+export function create_puzzle(a: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
