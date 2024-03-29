2a1832b68cb5c9f4540ac1b5ff1db3082a7ea854
lukasvrenner <lukasvrenner@icloud.com>
2024-03-29 09:40:59 -0800

    initial support for clues

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import init, {create_puzzle, Orientation} from "./pkg/crossyword.js";
 init().then(() => {
     const puzzle = create_puzzle();
-    console.log(puzzle);
     drawPuzzle(puzzle);
 })
+
 function drawPuzzle(puzzle) {
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext('2d');
 
-    const boxSize = canvas.width / 40;
+    const boxSize = canvas.width / 20;
 
     ctx.fillStyle = '#FFFFFF';
     ctx.strokeStyle = '#000000';
@@ -17,14 +17,34 @@ function drawPuzzle(puzzle) {
 
         if (word.orientation == Orientation.Horizontal) {
             for (var j = 0; j < word.word.length; j ++) {
-                ctx.fillRect((word.xpos + j) * boxSize, boxSize * word.ypos, boxSize, boxSize);
-                ctx.strokeRect((word.xpos + j) * boxSize, boxSize * word.ypos, boxSize, boxSize);
+                ctx.fillRect(
+                    (word.xpos + j) * boxSize,
+                    boxSize * word.ypos, 
+                    boxSize, boxSize);
+
+                ctx.strokeRect(
+                    (word.xpos + j) * boxSize,
+                    boxSize * word.ypos, 
+                    boxSize, boxSize);
+
             }
+            // add clues
+            document.getElementById("horizontal-clues").innerHTML
+                += word.clue + "\n";
         } else {
             for (var j = 0; j < word.word.length; j ++) {
-                ctx.fillRect(word.xpos * boxSize, boxSize * (word.ypos + j), boxSize, boxSize);
-                ctx.strokeRect(word.xpos * boxSize, boxSize * (word.ypos + j), boxSize, boxSize);
+                ctx.fillRect(
+                    word.xpos * boxSize, 
+                    boxSize * (word.ypos + j), 
+                    boxSize, boxSize);
+
+                ctx.strokeRect(word.xpos * boxSize,
+                    boxSize * (word.ypos + j),
+                    boxSize, boxSize);
             }
+            // add clues
+            document.getElementById("vertical-clues").innerHTML
+                += word.clue + "\n";
         }
         
     }
