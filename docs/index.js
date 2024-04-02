a12d0db7ecb7fc6f1a064d0384ffdccd84587c60
lukasvrenner <lukasvrenner@icloud.com>
2024-04-02 06:21:38 -0800

    added ability to modify guesses. fixes #18

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,10 @@
 import init, {create_puzzle, Orientation} from "./wasm/crossyword.js";
 init().then(() => {
     const puzzle = create_puzzle();
-    drawPuzzle(puzzle);
-    drawClues(puzzle);
+    let guessedWords = Array(puzzle.length).fill("");
+
+    drawPuzzle(puzzle, guessedWords);
+    drawClues(puzzle, guessedWords);
     document.getElementById("answers-toggle").onclick = () => {
         if (document.getElementById("answers-toggle").innerHTML == "Show Answers") { 
             showAnswers(puzzle);
@@ -18,45 +20,77 @@ const ctx = canvas.getContext('2d');
 const boxSize = canvas.width / 20;
 ctx.font = "bold 9pt monospace";
 
-function drawPuzzle(puzzle) {
+function drawPuzzle(puzzle, guessedWords) {
     ctx.fillStyle = '#FFFFFF';
     ctx.strokeStyle = '#000000';
     for (const word of puzzle) {
 
         if (word.orientation == Orientation.Horizontal) {
-            for (var j = 0; j < word.word.length; j ++) {
+            for (var i = 0; i < word.word.length; i ++) {
                 ctx.fillRect(
-                    (word.xpos + j) * boxSize,
+                    (word.xpos + i) * boxSize,
                     boxSize * word.ypos, 
                     boxSize, boxSize);
 
                 ctx.strokeRect(
-                    (word.xpos + j) * boxSize,
+                    (word.xpos + i) * boxSize,
                     boxSize * word.ypos, 
                     boxSize, boxSize);
 
             }
         } else {
-            for (var j = 0; j < word.word.length; j ++) {
+            for (var i = 0; i < word.word.length; i ++) {
                 ctx.fillRect(
                     word.xpos * boxSize, 
-                    boxSize * (word.ypos + j), 
+                    boxSize * (word.ypos + i), 
                     boxSize, boxSize);
 
                 ctx.strokeRect(word.xpos * boxSize,
-                    boxSize * (word.ypos + j),
+                    boxSize * (word.ypos + i),
                     boxSize, boxSize);
             }
         }
     }
+    drawNumbers(puzzle);
+    drawGuesses(puzzle, guessedWords);
+}
+
+function drawNumbers(puzzle) {
     ctx.fillStyle = "#000000";
-    for (var i = 1; i <= puzzle.length; i ++) { 
-        const word = puzzle[i - 1];
-        ctx.fillText(i, word.xpos * boxSize, (word.ypos) * boxSize + 10);
+    for (var i = 0; i < puzzle.length; i ++) { 
+        const word = puzzle[i];
+        ctx.fillText(i + 1, word.xpos * boxSize, (word.ypos) * boxSize + 10);
     }
 }
 
-function drawClues(puzzle) {
+function drawGuesses(puzzle, guessedWords) {
+    ctx.fillStyle="#000000";
+
+    for(var i = 0; i < puzzle.length; i ++) {
+        const answer = puzzle[i];
+        const guess = guessedWords[i];
+
+        if (answer.orientation == Orientation.Horizontal) {
+            for (var j = 0; j < guess.length; j ++) {
+                ctx.fillText(
+                    guess[j], 
+                    (answer.xpos + 0.4 + j) * boxSize, 
+                    (answer.ypos + 0.6) * boxSize
+                );
+            }
+        } else {
+            for (var j = 0; j < guess.length; j ++) {
+                ctx.fillText(
+                    guess[j], 
+                    (answer.xpos + 0.4) * boxSize,
+                    (answer.ypos + 0.6 + j) * boxSize
+                );
+            }
+        }
+    }
+}
+
+function drawClues(puzzle, guessedWords) {
     for (var i = 0; i < puzzle.length; i ++) {
         const word = puzzle[i];
 
@@ -65,11 +99,14 @@ function drawClues(puzzle) {
 
         const inputField = document.createElement("input");
         inputField.setAttribute("maxlength", word.word.length);
-        inputField.addEventListener("keydown", function (e) {
-            if (e.code === "Enter") {
-                showGuess(inputField.value, word);
-            }
-        });
+        (i => {
+            inputField.addEventListener("keydown", function (e) {
+                if (e.code === "Enter") {
+                    guessedWords[i] = inputField.value;
+                    drawPuzzle(puzzle, guessedWords);
+                }
+            });
+        }) (i)
 
         if (word.orientation == Orientation.Vertical) {
             document.getElementById("vertical").appendChild(clue)
@@ -110,23 +147,3 @@ function hideAnswers(puzzle) {
     drawPuzzle(puzzle);
 }
 
-function showGuess(guess, word) {
-    ctx.fillStyle="#000000";
-    if (word.orientation == Orientation.Horizontal) {
-        for (var i = 0; i < guess.length; i ++) {
-            ctx.fillText(
-                guess[i], 
-                (word.xpos + 0.4 + i) * boxSize, 
-                (word.ypos + 0.6) * boxSize
-            );
-        }
-    } else {
-        for (var i = 0; i < guess.length; i ++) {
-            ctx.fillText(
-                guess[i], 
-                (word.xpos + 0.4) * boxSize,
-                (word.ypos + 0.6 + i) * boxSize
-            );
-        }
-    }
-}
