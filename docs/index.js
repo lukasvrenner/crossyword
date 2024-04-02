c0d66a5c69a881247171ca0e0f1e1810360e3845
lukasvrenner <lukasvrenner@icloud.com>
2024-04-01 19:19:49 -0800

    added support for inputting words

--- docs/index.js
+++ docs/index.js
@@ -19,10 +19,9 @@ const boxSize = canvas.width / 20;
 ctx.font = "bold 9pt monospace";
 
 function drawPuzzle(puzzle) {
+    ctx.fillStyle = '#FFFFFF';
     ctx.strokeStyle = '#000000';
-    for (var i = 0; i < puzzle.length; i ++) {
-        const word = puzzle[i];
-        ctx.fillStyle = '#FFFFFF';
+    for (const word of puzzle) {
 
         if (word.orientation == Orientation.Horizontal) {
             for (var j = 0; j < word.word.length; j ++) {
@@ -49,20 +48,35 @@ function drawPuzzle(puzzle) {
                     boxSize, boxSize);
             }
         }
-        ctx.fillStyle = "#000000";
-        ctx.fillText(i + 1, word.xpos * boxSize, (word.ypos) * boxSize + 10);
+    }
+    ctx.fillStyle = "#000000";
+    for (var i = 1; i <= puzzle.length; i ++) { 
+        const word = puzzle[i - 1];
+        ctx.fillText(i, word.xpos * boxSize, (word.ypos) * boxSize + 10);
     }
 }
 
 function drawClues(puzzle) {
     for (var i = 0; i < puzzle.length; i ++) {
         const word = puzzle[i];
+
+        const clue = document.createElement("p");
+        clue.innerText = (i + 1) + ". " + word.clue;
+
+        const inputField = document.createElement("input");
+        inputField.setAttribute("maxlength", word.word.length);
+        inputField.addEventListener("keydown", function (e) {
+            if (e.code === "Enter") {
+                showGuess(inputField.value, word);
+            }
+        });
+
         if (word.orientation == Orientation.Vertical) {
-            document.getElementById("vertical-clues").innerHTML
-                += (i + 1) + ". " + word.clue + "<br>";
+            document.getElementById("vertical").appendChild(clue)
+            document.getElementById("vertical").appendChild(inputField);
         } else {
-            document.getElementById("horizontal-clues").innerHTML
-                += (i + 1) + ". " + word.clue + "<br>";
+            document.getElementById("horizontal").appendChild(clue)
+            document.getElementById("horizontal").appendChild(inputField);
         }
     }
 }
@@ -94,4 +108,25 @@ function showAnswers(puzzle) {
 function hideAnswers(puzzle) {
     document.getElementById("answers-toggle").innerHTML = "Show Answers"
     drawPuzzle(puzzle);
+}
+
+function showGuess(guess, word) {
+    ctx.fillStyle="#000000";
+    if (word.orientation == Orientation.Horizontal) {
+        for (var i = 0; i < word.word.length; i ++) {
+            ctx.fillText(
+                guess[i], 
+                (word.xpos + 0.4 + i) * boxSize, 
+                (word.ypos + 0.6) * boxSize
+            );
+        }
+    } else {
+        for (var i = 0; i < word.word.length; i ++) {
+            ctx.fillText(
+                guess[i], 
+                (word.xpos + 0.4) * boxSize,
+                (word.ypos + 0.6 + i) * boxSize
+            );
+        }
+    }
 }
